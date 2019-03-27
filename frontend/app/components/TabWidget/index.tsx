ad2773fef58f1e5397bfea0b1007ffd23de78db6
Pavel Zinin <pzinin@gmail.com>
2019-03-27 21:30:57 +0300

    Frontend, Create a new TabWidget thus encapsulating tab logic. VMSettings uses TabWidget

---
+++ frontend/app/components/TabWidget/index.tsx
@@ -0,0 +1,70 @@
+import React, {ReactNode, Fragment, useCallback, useState, useMemo, FunctionComponent, useEffect} from "react";
+import {Nav, NavLink, NavItem, TabContent, TabPane} from "reactstrap";
+import classnames from "classnames";
+
+export interface Tab {
+  header: ReactNode;
+  content: ReactNode;
+  tabDisabled?: boolean;
+}
+
+type TKey = string | number;
+
+export interface TabWidgetProps {
+  tabs: Map<TKey, Tab>;
+  onAfterActivation: (activeTab: TKey) => void;
+  header: ReactNode;
+  defaultTab: TKey;
+  /**
+   * If not null, turns this into a controlled component
+   */
+  tab?: TKey;
+}
+
+export const TabWidget: FunctionComponent<TabWidgetProps> =
+  ({tabs, onAfterActivation, header, defaultTab, tab}: TabWidgetProps) => {
+    const [activeTab, setActiveTab] = useState(defaultTab);
+    const tabEntries = useMemo(() => [...tabs.entries()], [tabs]);
+    const toggleTab = useCallback((tab: TKey) => {
+      if (activeTab !== tab) {
+        setActiveTab(tab);
+        onAfterActivation(tab);
+      }
+    }, [activeTab, onAfterActivation]);
+    useEffect(() => {
+      if (tab) {
+        setActiveTab(tab);
+        onAfterActivation(tab);
+      }
+    }, [tab]);
+    return (
+      <div>
+        {header}
+        <Nav tabs={true}>
+          {tabEntries.map(([key, value]) => (
+              <NavItem>
+                <NavLink
+                  className={classnames({active: activeTab === key})}
+                  onClick={() => {
+                    toggleTab(key);
+                  }}
+                  disabled={value.tabDisabled}
+                >
+                  {value.header}
+                </NavLink>
+              </NavItem>
+            )
+          )
+          }
+        </Nav>
+        <TabContent activeTab={activeTab}>
+          {tabEntries.map(([key, value]) => (
+            <TabPane tabId={key}>
+              {value.content}
+            </TabPane>
+          ))
+          }
+        </TabContent>
+      </div>
+    )
+  };
--- frontend/app/components/VMSettingsForm/index.tsx
+++ frontend/app/components/VMSettingsForm/index.tsx
@@ -4,7 +4,7 @@
  *
  */
 
-import React, {useCallback, useState} from 'react';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
 import Overview from './subforms/overview';
 import {Badge, Col, Nav, NavItem, NavLink, Row, TabContent, TabPane} from 'reactstrap';
 import classnames from 'classnames';
@@ -24,6 +24,7 @@ import {
   VMInfoFragmentFragment,
   VMInfoQuery
 } from "../../generated-models";
+import {TabWidget, TabWidgetProps} from "../TabWidget";
 
 
 enum Tab {
@@ -37,108 +38,56 @@ enum Tab {
 
 type VM = Omit<VMInfoFragmentFragment, "__typename">;
 
-const VMSettingsForm: React.FunctionComponent<SettingsComponentProps<VMInfoQuery>> = ({object}) => {
-  const vm: VM = object.vm;
-  const [activeTab, setActiveTab] = useState(Tab.Overview);
+const VMSettingsForm: React.FunctionComponent<SettingsComponentProps<VMInfoQuery>> = ({object: {vm}}) => {
+  const defaultTab = Tab.Overview;
+  const [overrideTab, setOverrideTab] = useState(null);
   const [vncActivated, setVncActivated] = useState(false);
-
-  const toggleTab = useCallback((tab: Tab) => {
-    if (activeTab !== tab) {
-      setActiveTab(tab);
+  const [activeTab, setActiveTab] = useState(defaultTab);
+  const onAfterActivation = useCallback((tab: Tab) => {
+    setActiveTab(tab);
+    if (tab === Tab.VNC) {
+      if (!vncActivated) {
+        setVncActivated(true);
+      }
     }
-    if (tab === Tab.VNC && !vncActivated) {
-      setVncActivated(true);
+    setOverrideTab(null);
+  }, [vncActivated]);
+
+  const changeVNCIfNotRunning = useEffect(() => {
+    if (vm.powerState !== PowerState.Running) {
+      if (activeTab === Tab.VNC) {
+        setOverrideTab(defaultTab);
+      }
     }
-  }, [activeTab, vncActivated]);
+  }, [vm.powerState, activeTab]);
 
-  if (activeTab === Tab.VNC && vm.powerState !== PowerState.Running) {
-    setVncActivated(false);
-    setActiveTab(Tab.Overview);
-  }
-  return (
-    <div>
-      <XenObjectHeader
-        xenObject={vm}
-        editMutationName={"object"}
-        editMutation={VMEditOptionsDocument}
-      >
-        <Badge color="primary">{vm.powerState}</Badge>
-        {vm.osVersion &&
-        (<Badge color="success">{vm.osVersion.name}</Badge>)}
-      </XenObjectHeader>
+  const header = useMemo(() => (
+    <XenObjectHeader
+      xenObject={vm}
+      editMutationName={"object"}
+      editMutation={VMEditOptionsDocument}
+    >
+      <Badge color="primary">{vm.powerState}</Badge>
+      {vm.osVersion &&
+      (<Badge color="success">{vm.osVersion.name}</Badge>)}
+    </XenObjectHeader>
+  ), [vm.nameLabel, vm.nameDescription]);
 
-      <Nav tabs={true}>
-        <NavItem>
-          <NavLink
-            className={classnames({active: activeTab === Tab.Overview})}
-            onClick={() => {
-              toggleTab(Tab.Overview);
-            }}
-          >
-            Overview
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            className={classnames({active: activeTab === Tab.CPU})}
-            onClick={() => {
-              toggleTab(Tab.CPU);
-            }}
-          >
-            CPU & Memory
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            className={classnames({active: activeTab === Tab.VNC})}
-            onClick={() => {
-              toggleTab(Tab.VNC);
-            }}
-            disabled={vm.powerState !== PowerState.Running || !vm.myActions.includes(VMActions.VNC)}
-          >
-            VNC
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            className={classnames({active: activeTab === Tab.Access})}
-            onClick={() => {
-              toggleTab(Tab.Access);
-            }}
-          >
-            Access
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            className={classnames({active: activeTab === Tab.Storage})}
-            onClick={() => {
-              toggleTab(Tab.Storage);
-            }}
-          >
-            Storage
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            className={classnames({active: activeTab === Tab.Network})}
-            onClick={() => {
-              toggleTab(Tab.Network);
-            }}
-          >
-            Network
-          </NavLink>
-        </NavItem>
-      </Nav>
-      <TabContent activeTab={activeTab}>
-        <TabPane tabId={Tab.Overview}>
+  const tabs: TabWidgetProps["tabs"] = new Map(
+    [
+      [Tab.Overview, {
+        header: "Overview",
+        content: (
           <Row>
             <Col sm="12">
               <Overview vm={vm}/>
             </Col>
           </Row>
-        </TabPane>
-        <TabPane tabId={Tab.CPU}>
+        )
+      }],
+      [Tab.CPU, {
+        header: "CPU & Memory",
+        content: (
           <Row>
             <Col sm="12">
               <ResourcesFormContainer
@@ -149,8 +98,28 @@ const VMSettingsForm: React.FunctionComponent<SettingsComponentProps<VMInfoQuery
               />
             </Col>
           </Row>
-        </TabPane>
-        <TabPane tabId={Tab.Access}>
+        )
+      }
+      ],
+      [Tab.VNC, {
+        header: "VNC",
+        tabDisabled: useMemo(() =>
+          vm.powerState !== PowerState.Running || !vm.myActions.includes(VMActions.VNC),
+          [vm.powerState, vm.myActions]),
+        content: useMemo(() => {
+          if (vncActivated)
+            return (
+              <Vncview vm={vm}/>
+            );
+          else {
+            return (<h1>NO VNC HERE</h1>);
+          }
+
+        }, [vncActivated]),
+      }],
+      [Tab.Access, {
+        header: "Access",
+        content: (
           <Row>
             <Col sm="12">
               {<AccessView
@@ -161,23 +130,22 @@ const VMSettingsForm: React.FunctionComponent<SettingsComponentProps<VMInfoQuery
               />}
             </Col>
           </Row>
-        </TabPane>
-        <TabPane tabId={Tab.Storage}>
+        )
+      }],
+      [Tab.Storage, {
+        header: "Storage",
+        content: (
           <Row>
             <Col sm="12">
               {<Storage
                 vm={vm}/>}
             </Col>
           </Row>
-        </TabPane>
-        <TabPane tabId={Tab.VNC}>
-          {
-            vncActivated && (
-              <Vncview vm={vm}/>
-            ) || (<h1>NO VNC HERE</h1>)
-          }
-        </TabPane>
-        <TabPane tabId={Tab.Network}>
+        )
+      }],
+      [Tab.Network, {
+        header: "Network",
+        content: (
           <Row>
             <Col sm="12">
               {<Network
@@ -185,9 +153,19 @@ const VMSettingsForm: React.FunctionComponent<SettingsComponentProps<VMInfoQuery
               />}
             </Col>
           </Row>
-        </TabPane>
-      </TabContent>
-    </div>
+        )
+      }]
+    ]
+  );
+
+  return (
+    <TabWidget
+      tabs={tabs}
+      onAfterActivation={onAfterActivation}
+      header={header}
+      defaultTab={defaultTab}
+      tab={overrideTab}
+    />
   )
 };
 
